f65c875735a5294319aea8f44f52d6327fd5bd52
Stephanefy <stephanefokyin@gmail.com>
2022-03-21 18:41:23 +0400

    fixed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/img/logo/jeunesse-top-illustration.svg`, // This path is relative to the root of the site.
+        icon: `src/assets/img/logo/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
